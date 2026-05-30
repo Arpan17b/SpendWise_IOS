fdaebb82de6b0b0e2bf2195b898dd27651c6f03f
Arpan Dutta <arpanndutta@gmail.com>
2026-05-30 11:33:34 +0530

    Initial Commit

---
+++ First_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First_App
+//
+//  Created by Arpan Dutta on 30/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First_App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First_App
+//
+//  Created by Arpan Dutta on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
